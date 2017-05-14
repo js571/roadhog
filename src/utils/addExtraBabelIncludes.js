8b8fb40e963746162776496e7621095122b391a4
maat <maat@ixiaopu.com>
2017-05-14 21:54:56 +0800

    .

--- src/utils/addExtraBabelIncludes.js
+++ src/utils/addExtraBabelIncludes.js
@@ -2,13 +2,10 @@ import { join } from 'path';
 
 export default function (webpackConfig, paths, includes = []) {
   includes.forEach((include) => {
-  	const extraPath = include.path;
-    const loader = include.loader || 'babel';
-    const test = include.test || /\.(js|jsx)$/;
     webpackConfig.module.loaders.push({
-      test,
-      include: join(paths.appDirectory, extraPath),
-      loader,
+      test: /\.(js|jsx)$/,
+      include: join(paths.appDirectory, include),
+      loader: 'babel',
     });
   });
   return webpackConfig;
